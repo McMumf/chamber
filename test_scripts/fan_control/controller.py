7854b0cdeb85f42756d460acc37f8fbff969614e
Carson <mccaffreycarson@gmail.com>
2024-08-11 11:33:15 -0400

    Initial fan controller script

--- test_scripts/fan_control/controller.py
+++ test_scripts/fan_control/controller.py
@@ -1,21 +1,48 @@
 
-import os
-
-import signal
-import sys
 import RPi.GPIO as GPIO
+import time
 
-pin = 26
+"""
+NT07-115X Wiring
+    - Pins
+        - 1: Black
+        - 2: Red
+        - 3: Yellow
+        - 4: Blue
+    - Function
+        - 1: Ground
+        - 2: +12v/5v
+        - 3: Tach/Signal/Sense
+        - 4: Control/PWM
+"""
+
+pwm_pin_num = 12
 
 def setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin, GPIO.OUT)
     GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pwm_pin_num, GPIO.OUT)
 
-def fan_on():
-    setPin(True)
+pwm = GPIO.PWM(pwm_pin_num, 25000)
+pwm.stat(0)
 
-def fan_off():
-    setPin(False)
+time.sleep(2)
 
+try:
+    while True:
+        pwm.ChangeDutyCycle(100)
+        print('Duty Cycle: 100')
+        time.sleep(5)
+        pwm.ChangeDutyCycle(50)
+        print('Duty Cycle: 50')
+        time.sleep(5)
+        pwm.ChangeDutyCycle(0)
+        print('Duty Cycle: 0')
+        time.sleep(5)
 
+except KeyboardInterrupt:
+    pass
+
+pwm.ChangeDutyCycle(0)
+pwm.stop()
+GPIO.cleanup()
