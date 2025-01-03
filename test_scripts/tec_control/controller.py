add6180a00c525405e639945095aeb53f4c8d5ad
Carson <carson@mcmumf.dev>
2025-01-03 10:22:41 -0500

    Attempting tec control

---
+++ test_scripts/tec_control/controller.py
@@ -0,0 +1,42 @@
+
+import RPi.GPIO as GPIO
+import time
+
+"""
+Individual Control for TEC1/Peltier
+
+MD10C R3 to GPIO Wiring
+  - GND -> GND
+  - PWM -> 5.0v
+  - DIR -> 19
+"""
+
+pwm_gpio_pin_num = 19 # this will be GPIO Pin 19, the physical pin number is 35
+
+def setup():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pwm_gpio_pin_num, GPIO.OUT)
+
+setup()
+
+pwm = GPIO.PWM(pwm_gpio_pin_num, 25000)
+pwm.start(0)
+
+time.sleep(2)
+
+try:
+    while True:
+        pwm.ChangeDutyCycle(100)
+        print('Duty Cycle: 100')
+        time.sleep(5)
+        pwm.ChangeDutyCycle(0)
+        print('Duty Cycle: 0')
+        time.sleep(30)
+
+except KeyboardInterrupt:
+    pass
+
+pwm.ChangeDutyCycle(0)
+pwm.stop()
+GPIO.cleanup()
