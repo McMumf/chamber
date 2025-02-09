42122fbcfc62b2c4f31ae7200dfe29cf5a678e92
Carson <carson@mcmumf.dev>
2025-02-08 19:46:08 -0500

    Changeout to ds18b20

--- src/application.cpp
+++ src/application.cpp
@@ -13,10 +13,11 @@
 #include <thread>
 #include <vector>
 
-#include "./Adafruit_ADS1X15/Adafruit_ADS1X15.h"
 #include "Adafruit_GFX.h"
 #include "ArduiPi_OLED.h"
 #include "ArduiPi_OLED_lib.h"
+#include <bcm2835.h>
+#include "./ds18b20/ds18b20.h"
 #include "pigpio.h"
 
 using namespace std::literals::chrono_literals;
@@ -33,7 +34,7 @@ std::chrono::system_clock::time_point start_time;
 std::chrono::duration<double> diff;
 
 volatile sig_atomic_t stop;
-double control_cycle = 0.500;  // Seconds
+double control_cycle = 0.500; // Seconds
 int temp_target = 23;
 std::vector<float> temps(8, 0);
 unsigned int ModeGpio = 23;
@@ -46,68 +47,16 @@ unsigned int PeltierPWM = 19;
 void inthand(int signum) { stop = 1; }
 
 /**
- * Get one reading from Adafruit ADC1115.
- *
- * @param Channel - the ADC channel to read from
- * @returns ADC Reading (range: 0 - 1023)
- */
-int get_a2d_val(int Channel) {
-  mcp3008Spi a2d("/dev/spidev0.0", SPI_MODE_0, 1000000, 8);
-  int a2dVal = 0;
-  unsigned char data[3];
-
-  data[0] = 1;  // First byte -> start bit
-  data[1] = 0b10000000 |
-            (((Channel & 7) << 4));  // Second byte -> (SGL/DIF = 1, D2=D1=D0=0)
-  data[2] = 0;                       // Third byte....don't care
-
-  a2d.spiWriteRead(data, sizeof(data));
-
-  a2dVal =
-      (data[1] << 8) & 0b1100000000;  // Merge data[1] & data[2] to get result
-  a2dVal |= (data[2] & 0xff);
-
-  return a2dVal;
-}
-
-/**
- * Temperature in Celsius measured with an 
+ * Temperature in Celsius measured with an
  *
  * @returns temperature in Celsius
  */
-float get_temp(int channel = 0) {
-  float temp_K, v_t, r_t;
-  double r_norm;
+float get_temp(int channel = 0)
+{
 
-  // Resistance at 25 deg C taken from:
-  // https://www.amphenol-sensors.com/en/component/edocman/328-thermometrics-ntc-thermistors-epoxy-interchangeable-type-95-datasheet/viewdocument
-  const float R_25 = 2252;
-  const float V_REF = 3.3;     // Voltage divider and logic reference
-  const float R_f = 1100;      // Resistance of voltage divider resistor
-  const float T_K_0 = 273.15;  // Temp in Kelvin at 0 deg C
+  readTemp()
 
-  // Steinhart–Hart equation coefficients taken from material Type F,
-  // resistances 3.274 to 0.36036 Ohms:
-  // https://www.amphenol-sensors.com/en/component/edocman/292-thermometrics-temperature-resistance-curves-reference-guide/viewdocument
-  const double A = 3.3540154E-03;
-  const double B = 2.5627725E-04;
-  const double C = 2.0829210E-06;
-  const double D = 7.3003206E-08;
-
-  // Get reading from ADC.
-  int a2dVal = get_a2d_val(channel);
-
-  v_t = a2dVal * V_REF / 1023;      // Input voltage at temp
-  r_t = R_f * v_t / (V_REF - v_t);  // Thermistor resistance at temp
-  r_norm = log(r_t / R_25);
-
-  // Temperature in Kelvin.
-  temp_K = 1 / (A + B * r_norm + C * pow(r_norm, 2) + D * pow(r_norm, 3));
-
-  // Convert temp to Celsius.
-  float temp = temp_K - T_K_0;
-
-  return temp;
+      return temp;
 }
 
 // Convert temperature from Celsius to Fahrenheit.
@@ -125,7 +74,8 @@ float celsius_to_fahrenheit(float temp_C) { return (temp_C * 9.0 / 5.0 + 32); }
  * @param string - text to display
  * @returns 0 if success
  */
-int display_text(const char *string) {
+int display_text(const char *string)
+{
   const char *text = string;
 
   display.clearDisplay();
@@ -141,12 +91,13 @@ int display_text(const char *string) {
 }
 
 // Initialize OLED display.
-int display_init(void) {
+int display_init(void)
+{
   // Configure display model and pins.
-  const int OLED_TYPE = 0;        // OLED_ADAFRUIT_SPI_128x32
-  const int OLED_SPI_DC = 6;      // RPI GPIO 6 pin 31
-  const int OLED_SPI_RESET = 26;  // RPI GPIO 26 pin 37
-  const int OLED_SPI_CS = 1;      // BCM2835_SPI_CS1
+  const int OLED_TYPE = 0;       // OLED_ADAFRUIT_SPI_128x32
+  const int OLED_SPI_DC = 6;     // RPI GPIO 6 pin 31
+  const int OLED_SPI_RESET = 26; // RPI GPIO 26 pin 37
+  const int OLED_SPI_CS = 1;     // BCM2835_SPI_CS1
 
   if (!display.init(OLED_SPI_DC, OLED_SPI_RESET, OLED_SPI_CS, OLED_TYPE))
     exit(EXIT_FAILURE);
@@ -156,15 +107,18 @@ int display_init(void) {
 }
 
 // Enforce the control cycle time.
-void dt_control() {
+void dt_control()
+{
   // Wait until main() sends data.
   std::unique_lock<std::mutex> lk(m);
-  cv.wait(lk, [] { return main_ready; });
+  cv.wait(lk, []
+          { return main_ready; });
 
   diff = std::chrono::system_clock::now() - start_time;
 
   // Wait until control cycle is up.
-  while (diff.count() < control_cycle) {
+  while (diff.count() < control_cycle)
+  {
     diff = std::chrono::system_clock::now() - start_time;
   }
 
@@ -177,22 +131,27 @@ void dt_control() {
 }
 
 // Increase temperature setpoint.
-void increase_setpoint(int gpio, int level, uint32_t tick) {
-  if (level == 0) {  // Only increment on the falling edge.
+void increase_setpoint(int gpio, int level, uint32_t tick)
+{
+  if (level == 0)
+  { // Only increment on the falling edge.
     temp_target++;
     std::cout << "GPIO triggered: " << gpio << std::endl;
   }
 }
 
 // Decrease temperature setpoint.
-void decrease_setpoint(int gpio, int level, uint32_t tick) {
-  if (level == 0) {  // Only decrement on the falling edge.
+void decrease_setpoint(int gpio, int level, uint32_t tick)
+{
+  if (level == 0)
+  { // Only decrement on the falling edge.
     temp_target--;
     std::cout << "GPIO triggered: " << gpio << std::endl;
   }
 }
 
-int main(void) {
+int main(void)
+{
   std::string mode;
   int chan = 0;
   int loop_count = 0;
@@ -200,30 +159,43 @@ int main(void) {
   float sum, temp = 0;
   int duty_cycle_inner, duty_cycle_peltier_outer, samples;
 
-  // ADC channels reading temperatures
-  std::vector<int> channels{0, 2};
-
   // Initialize display.
   display_init();
 
   // Initialize GPIOs.
-  if (gpioInitialise() < 0) return 1;
-  gpioSetMode(ModeGpio, PI_OUTPUT);     // Controls Peltier direction
-  gpioSetMode(incInputGpio, PI_INPUT);  // Increase setpoint.
-  gpioSetMode(decInputGpio, PI_INPUT);  // Decrease setpoint.
+  if (gpioInitialise() < 0)
+    return 1;
+  gpioSetMode(ModeGpio, PI_OUTPUT);    // Controls Peltier direction
+  gpioSetMode(incInputGpio, PI_INPUT); // Increase setpoint.
+  gpioSetMode(decInputGpio, PI_INPUT); // Decrease setpoint.
 
   // Callbacks for setpoint
   gpioSetAlertFunc(incInputGpio, increase_setpoint);
   gpioSetAlertFunc(decInputGpio, decrease_setpoint);
 
+  // Initialize bcm2835 library
+  if (!bcm2835_init())
+  {
+    cerr << "Initialize BCM2835 failed!" << endl;
+
+    return -1;
+  }
+
+  // Set GPIO 4 to input with pullup
+  bcm2835_gpio_fsel(4, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_set_pud(4, BCM2835_GPIO_PUD_UP);
+
+  DS18B20 ds18b20;
+
   // Set fixed duty cycles
-  duty_cycle_inner = 50;           // Inner fan
-  duty_cycle_peltier_outer = 100;  // Peltier and outer fan
+  duty_cycle_inner = 50;          // Inner fan
+  duty_cycle_peltier_outer = 100; // Peltier and outer fan
 
   // Handle CTRL-C interrupt from keyboard.
   signal(SIGINT, inthand);
 
-  while (!stop) {
+  while (!stop)
+  {
     main_ready = false;
     worker_ready = false;
 
@@ -236,63 +208,84 @@ int main(void) {
     }
     cv.notify_one();
 
-    // Update temperature reading.
-    temp = 0;
-    samples = 20;
-    for (int j = 0; j < samples; ++j) {
-      // Average temperatures from all channels.
-      for (std::size_t i = 0; i < channels.size(); ++i) {
-        chan = channels[i];
-        temps[chan] = get_temp(chan);
-      }
+    // Keeping this should I want to recreate this with ds18b20
+    // // Update temperature reading.
+    // temp = 0;
+    // samples = 20;
+    // for (int j = 0; j < samples; ++j)
+    // {
+    //   // Average temperatures from all channels.
+    //   for (std::size_t i = 0; i < channels.size(); ++i)
+    //   {
+    //     chan = channels[i];
+    //     temps[chan] = get_temp(chan);
+    //   }
 
-      sum = std::accumulate(temps.begin(), temps.end(), 0.0);
-      temp += (sum / channels.size());
-    }
+    //   sum = std::accumulate(temps.begin(), temps.end(), 0.0);
+    //   temp += (sum / channels.size());
+    // }
 
-    // Average temperatures from all samples.
-    temp = temp / samples;
+    // // Average temperatures from all samples.
+    // temp = temp / samples;
+
+    // Get temp in celsius from ds18b20
+    temp = ds18b20.readTemp();
 
     // If temperature too high, exit.
-    if (temp > 40) {
+    if (temp > 40)
+    {
       gpioHardwarePWM(PeltierPWM, 0, 0);
       printf("Temperature too high.");
       exit(EXIT_FAILURE);
     }
 
     // Don't update mode unless away from target temp or not in Rest
-    if ((std::abs(temp - temp_target) > 1) || mode != "REST") {
+    if ((std::abs(temp - temp_target) > 1) || mode != "REST")
+    {
       // Set direction and mode
-      if (temp > temp_target) {
-        if (mode == "HEAT") {
+      if (temp > temp_target)
+      {
+        if (mode == "HEAT")
+        {
           flip_flop++;
-        } else {
+        }
+        else
+        {
           gpioWrite(23, 1);
           mode = "COOL";
         }
-      } else {
-        if (mode == "COOL") {
+      }
+      else
+      {
+        if (mode == "COOL")
+        {
           flip_flop++;
-        } else {
+        }
+        else
+        {
           gpioWrite(23, 0);
           mode = "HEAT";
         }
       }
     }
 
-    if (flip_flop > 0 || mode == "REST") {
+    if (flip_flop > 0 || mode == "REST")
+    {
       // Disable fans and Peltier near target temp.
       gpioHardwarePWM(FanPWM, 0, 0);
       gpioHardwarePWM(PeltierPWM, 0, 0);
       flip_flop = 0;
       mode = "REST";
-    } else {
+    }
+    else
+    {
       gpioHardwarePWM(FanPWM, 5E5, (10000 * duty_cycle_inner));
       gpioHardwarePWM(PeltierPWM, 5E5, (10000 * duty_cycle_peltier_outer));
     }
 
     // Display and print temperature at reduced loop rate.
-    if (loop_count == 0 || loop_count % 10 == 0) {
+    if (loop_count == 0 || loop_count % 10 == 0)
+    {
       std::string text =
           "Temp: " + std::to_string(static_cast<int>(round(temp))) + (char)247 +
           "C | " + mode + "ING\n" + "     (" + std::to_string(temp_target) +
@@ -309,7 +302,8 @@ int main(void) {
     // Wait for end of control cycle.
     {
       std::unique_lock<std::mutex> lk(m);
-      cv.wait(lk, [] { return worker_ready; });
+      cv.wait(lk, []
+              { return worker_ready; });
     }
     worker.join();
 
